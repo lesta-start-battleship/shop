04169c58fa5681c5a17faa33211fce2ff576aff1
A133uz <matrix.valiev2005@gmail.com>
2025-06-25 20:05:08 +0000

    upd: urls.py был обновлен + добавлена модель для акций

--- apps/promotion/models.py
+++ apps/promotion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Promotion(models.Model):
+    name = models.CharField(max_length=255)
+    duration = models.DateTimeField()
+    items = models.IntegerField()
+    cost = models.FloatField()
+    quantity = models.IntegerField()
+    
