808463b56c8aa7d5c5d654c04ae3caf3481d74d6
MuhabillohHakimjonov <mukhabillo@gmail.com>
2025-06-26 19:38:43 +0500

    fixed bugs

--- apps/purchase/views.py
+++ apps/purchase/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Purchase
-from .apps import PurchaseSerializer
+from .serializers import PurchaseSerializer
 
 @api_view(["GET"])
 def list_purchases(request):
