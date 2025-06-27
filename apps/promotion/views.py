2acf9ce868948c770ec62486842365921e2735dc
A133uz <matrix.valiev2005@gmail.com>
2025-06-27 19:22:26 +0000

    services.py was added for promotions and the implemetation of compensation has been started

--- apps/promotion/views.py
+++ apps/promotion/views.py
@@ -1,5 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action 
+from rest_framework import status
+from .services import compensate_unopened_chests
+
 from .models import Promotion
 from .serializers import PromotionSerializer
 
@@ -8,3 +12,7 @@ class PromotionViewSet(ModelViewSet):
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
     permission_classes = [IsAdminUser]
+    
+    @action(detail=True, methods=["post"], permission_classes=[IsAdminUser])
+    def compensate(self, request, pk=None):
+        pass
