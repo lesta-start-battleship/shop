37b985985ce3bfa04a08118472621881c9e91070
A133uz <matrix.valiev2005@gmail.com>
2025-06-27 17:15:18 +0000

    admin.py was changed

--- apps/promotion/models.py
+++ apps/promotion/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Promotion(models.Model):
-    name = models.CharField(max_length=255)
-    duration = models.DateTimeField()
-    items = models.IntegerField()
-    cost = models.FloatField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+    item_ids = models.JSONField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        end_time = self.start_time + self.duration
+        return f"{self.name} | {self.start_time.date()} → {end_time.date()} | Price: {self.price}"
+        
     
