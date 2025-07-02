8c153dc115abbda3aae8023d96e840e22f5ed261
sendy-tech <1dembel@mail.ru>
2025-07-02 23:43:17 +0300

    try correct kafka logic to use saga

--- kafka/handlers.py
+++ kafka/handlers.py
@@ -1,8 +1,7 @@
 import logging
-from django.db import transaction
 
 from kafka.producer import send_kafka_message
-from apps.purchase.models import Purchase, TransactionStatus
+from apps.saga.models import SagaOrchestrator
 
 logger = logging.getLogger(__name__)
 
@@ -11,93 +10,89 @@ def handle_auth_reserve_result(event: dict):
     """
     Обрабатывает результат резервирования средств из auth-сервиса.
     """
-    purchase_id = event.get("purchase_id")
     success = event.get("success")
     transaction_id = event.get("transaction_id")
 
-    if not all([purchase_id, transaction_id]):
+    if not all([transaction_id, transaction_id]):
         logger.error(f"[Kafka] Missing required fields in auth-reserve-result: {event}")
         return
 
     try:
-        purchase = Purchase.objects.get(id=purchase_id)
-    except Purchase.DoesNotExist:
-        logger.error(f"[Kafka] Purchase with id {purchase_id} not found")
+        sagaOrchestrator = SagaOrchestrator.objects.get(id=transaction_id)
+    except SagaOrchestrator.DoesNotExist:
+        logger.error(f"[Kafka] Purchase with id {transaction_id} not found")
         return
 
     if success:
-        purchase.transaction_status = TransactionStatus.RESERVED
-        purchase.save(update_fields=["transaction_status"])
-        logger.info(f"[Kafka] Purchase {purchase_id} -> RESERVED")
+        sagaOrchestrator.transaction_status = SagaOrchestrator.STATUS_CHOICES[0]
+        sagaOrchestrator.save(update_fields=["transaction_status"])
+        logger.info(f"[Kafka] Transaction {transaction_id} -> RESERVED")
 
         send_kafka_message("inventory-add", {
-            "user_id": purchase.owner,
-            "product_id": purchase.item.id,
-            "transaction_id": transaction_id,
-            "purchase_id": purchase.id
+            "user_id": sagaOrchestrator.user_id,
+            "product_id": sagaOrchestrator.item.id,
+            "transaction_id": transaction_id
         })
-        logger.info(f"[Kafka] Sent inventory-add for purchase {purchase.id}")
+        logger.info(f"[Kafka] Sent inventory-add for transaction {transaction_id}")
     else:
-        purchase.transaction_status = TransactionStatus.DECLINED
-        purchase.save(update_fields=["transaction_status"])
-        logger.warning(f"[Kafka] Purchase {purchase_id} -> DECLINED")
+        sagaOrchestrator.transaction_status = SagaOrchestrator.STATUS_CHOICES[2]
+        sagaOrchestrator.save(update_fields=["transaction_status"])
+        logger.warning(f"[Kafka] sagaOrchestrator {transaction_id} -> DECLINED")
 
 
 def handle_inventory_result(event: dict):
     """
     Обрабатывает результат добавления товара в инвентарь.
     """
-    purchase_id = event.get("purchase_id")
     success = event.get("success")
     transaction_id = event.get("transaction_id")
 
-    if not all([purchase_id, transaction_id]):
+    if not all(transaction_id):
         logger.error(f"[Kafka] Missing required fields in inventory-result: {event}")
         return
 
     try:
-        purchase = Purchase.objects.get(id=purchase_id)
-    except Purchase.DoesNotExist:
-        logger.error(f"[Kafka] Purchase with id {purchase_id} not found")
+        sagaOrchestrator = SagaOrchestrator.objects.get(id=transaction_id)
+    except SagaOrchestrator.DoesNotExist:
+        logger.error(f"[Kafka] Transaction with id {transaction_id} not found")
         return
 
     if success:
         send_kafka_message("auth-commit", {
-            "transaction_id": transaction_id,
-            "purchase_id": purchase.id
+            "transaction_id": transaction_id
         })
-        logger.info(f"[Kafka] Sent auth-commit for purchase {purchase.id}")
+        logger.info(f"[Kafka] Sent auth-commit for transaction {transaction_id}")
     else:
-        purchase.transaction_status = TransactionStatus.FAILED
-        purchase.save(update_fields=["transaction_status"])
-        logger.warning(f"[Kafka] Purchase {purchase.id} -> FAILED (inventory error)")
+        sagaOrchestrator.transaction_status = SagaOrchestrator.STATUS_CHOICES[2]
+        sagaOrchestrator.save(update_fields=["transaction_status"])
+        logger.warning(f"[Kafka] Transaction {transaction_id} -> FAILED (inventory error)")
 
 
 def handle_auth_commit_result(event: dict):
     """
     Обрабатывает результат финального списания средств.
     """
-    purchase_id = event.get("purchase_id")
+    transaction_id = event.get("transaction_id")
     success = event.get("success")
 
-    if not purchase_id:
-        logger.error(f"[Kafka] Missing purchase_id in auth-commit-result: {event}")
+    if not transaction_id:
+        logger.error(f"[Kafka] Missing transaction_id in auth-commit-result: {event}")
         return
 
     try:
-        purchase = Purchase.objects.get(id=purchase_id)
-    except Purchase.DoesNotExist:
-        logger.error(f"[Kafka] Purchase with id {purchase_id} not found")
+        sagaOrchestrator = SagaOrchestrator.objects.get(id=transaction_id)
+    except SagaOrchestrator.DoesNotExist:
+        logger.error(f"[Kafka] Purchase with id {transaction_id} not found")
         return
 
     if success:
-        purchase.transaction_status = TransactionStatus.COMPLETED
-        logger.info(f"[Kafka] Purchase {purchase.id} -> COMPLETED")
+        sagaOrchestrator.transaction_status = SagaOrchestrator.STATUS_CHOICES[1]
+        logger.info(f"[Kafka] Transaction {transaction_id} -> COMPLETED")
     else:
-        purchase.transaction_status = TransactionStatus.FAILED
-        logger.warning(f"[Kafka] Purchase {purchase.id} -> FAILED (auth commit)")
+        sagaOrchestrator.transaction_status = SagaOrchestrator.STATUS_CHOICES[2]
+        logger.warning(f"[Kafka] Transaction {transaction_id} -> FAILED (auth commit)")
 
-    purchase.save(update_fields=["transaction_status"])
+    sagaOrchestrator.save(update_fields=["status"])
 
 
 def handle_guild_war_game(event: dict):
