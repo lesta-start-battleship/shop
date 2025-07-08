6928428a425f3a21f3faa9ad5866bc245a98e0a7
Dvaraz <withoutme1988@gmail.com>
2025-07-08 16:05:12 +0300

    added: open chest black pearl

--- apps/chest/serializers.py
+++ apps/chest/serializers.py
@@ -5,6 +5,7 @@ from apps.chest.models import Chest
 
 class ChestSerializer(serializers.ModelSerializer):
     products = serializers.SerializerMethodField()
+    special_products = serializers.SerializerMethodField()
 
     class Meta:
         model = Chest
@@ -17,12 +18,17 @@ class ChestSerializer(serializers.ModelSerializer):
             "currency_type",
             "cost",
             "experience",
-            "products"
+            "products",
+            "special_products"
         ]
 
     def get_products(self, obj):
         from apps.product.serializers import ProductSerializer
         return ProductSerializer(obj.product.all(), many=True, context=self.context).data
+
+    def get_special_products(self, obj):
+        from apps.product.serializers import ProductSerializer
+        return ProductSerializer(obj.special_products.all(), many=True, context=self.context).data
 
 
 class ChestOpenSerializer(serializers.Serializer):
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib import admin
 
 schema_view = get_schema_view(
 	openapi.Info(
@@ -17,7 +18,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-	path('admin/', include('apps.custom_admin.urls')),
+	# path('admin/', include('apps.custom_admin.urls')),
+	path('admin/', admin.site.urls),
 	path('item/', include('apps.product.urls')),
 	path('promotion/', include('apps.promotion.urls')),
 	path('purchase/', include('apps.purchase.urls')),
