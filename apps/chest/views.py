ec3e0fff3dfdce10244b0898f05a75fa69cccde7
Dvaraz <withoutme1988@gmail.com>
2025-07-08 15:24:00 +0300

    added: open chest black pearl

--- apps/chest/views.py
+++ apps/chest/views.py
@@ -3,6 +3,7 @@ import logging
 import jwt
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
