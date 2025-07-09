7b761b572eacb2d14815383e392f2de0059e064d
Dvaraz <withoutme1988@gmail.com>
2025-07-10 02:02:14 +0300

    added: distribution

--- apps/chest/tasks.py
+++ apps/chest/tasks.py
@@ -1,12 +1,14 @@
 import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from datetime import timedelta
 
 import requests
 from celery import shared_task, group
+from django.utils import timezone
 from rest_framework.generics import get_object_or_404
 
-from apps.chest.models import Chest
+from apps.chest.models import Chest, ChestOpeningDistribution
 from apps.chest.utils import open_chest, generate_token, send_guild_war_reward, get_chest_settings
 from apps.saga.saga_orchestrator import get_producer
 from config import settings
@@ -19,52 +21,84 @@ GUILD_API_URL = settings.GUILD_API_URL
 
 
 @shared_task(bind=True, max_retries=3)
-def open_chest_task(self, chest_id: int, token, user_id, callback_url, amount: int = 1, ):
+def open_chest_task(self, distribution_id):
     logger.info("Open chest celery task started")
 
     try:
-        # Headers with received token
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"
-        }
+        distribution: ChestOpeningDistribution = ChestOpeningDistribution.objects.get(id=distribution_id)
+        distribution.retry += 1
+        distribution.save()
 
-        # Open chest in inventory
-        open_chest_url = f"{INVENTORY_API_URL}/inventory/use_item/"
-        open_response = requests.patch(
-            open_chest_url,
-            json={
-                "item_id": chest_id,
-                "amount": amount
-            },
-            headers=headers
-        )
+        if distribution.status == 'pending':
+            # Headers with received token
+            headers = {
+                "Authorization": f"Bearer {distribution.token}",
+                "Content-Type": "application/json"
+            }
 
-        if open_response.status_code == 422:
-            raise Exception("Validation Error")
-        elif open_response.status_code != 200:
-            raise Exception("Inventory service error")
+            # Open chest in inventory
+            open_chest_url = f"{INVENTORY_API_URL}/inventory/use_item/"
+            open_response = requests.patch(
+                open_chest_url,
+                json={
+                    "item_id": distribution.chest_id,
+                    "amount": distribution.amount
+                },
+                headers=headers
+            )
+            open_response.raise_for_status()
+            distribution.status = 'chest_used'
+            distribution.save()
 
-        # Open claimed chests
-        chest = Chest.objects.filter(item_id=chest_id).prefetch_related('product').first()
+        if distribution.status == 'chest_used':
+            # Open claimed chests
+            chest = Chest.objects.filter(item_id=distribution.chest_id).prefetch_related('product').first()
 
-        rewards = open_chest(chest, amount)
-        chests = rewards["promo_chests"]
+            rewards = open_chest(chest, distribution.amount)
+            distribution.rewards = rewards
+            distribution.status = 'rewards_generated'
+            distribution.save()
 
-        distribute_chest_opening_result(rewards, token)
-        if chests:
-            send_chest_event_to_kafka.delay(chests, user_id)
+        if distribution.status == 'rewards_generated':
+            send_to_inventory_service(distribution.rewards, distribution.token)
+
+            distribution.status = 'items_sent'
+            distribution.save()
+
+        if distribution.status == 'items_sent':
+            send_to_user_service(distribution.rewards)
+
+            distribution.status = 'gold_sent'
+            distribution.save()
+
+        if distribution.status == 'gold_sent':
+
+            if distribution.rewards.get('promo_chests'):
+                send_chest_event_to_kafka.delay(distribution.rewards['promo_chests'], distribution.user_id)
+
+            distribution.status = 'scoreboard_sent'
+            distribution.save()
 
         # if callback_url:
         #     send_callback.delay(rewards, callback_url, token)
 
+        if distribution.status == 'scoreboard_sent':
+            distribution.status = 'completed'
+
+            distribution.save()
+
     except Exception as e:
         logger.error(f"Error in open_chest_task: {str(e)}")
-        self.retry(exc=e, countdown=30)
+
+        distribution.status = 'failed'
+        distribution.save()
+
+        self.retry(exc=e)
 
 
-@shared_task(bind=True, max_retries=3)
-def send_to_inventory_service(self, result_data, token):
+
+@shared_task
+def send_to_inventory_service(result_data, token):
     """Celery task for send result about items to inventory"""
     headers = {
         "Authorization": f"Bearer {token}",
@@ -73,42 +107,34 @@ def send_to_inventory_service(self, result_data, token):
     inventory_response_url = f"{INVENTORY_API_URL}/inventory/add_item/"
 
     for item_id, amount in result_data["products_id"].items():
-        try:
-            requests.patch(
-                inventory_response_url,
-                json={
-                    "item_id": item_id,
-                    "amount": amount
-                },
-                headers=headers
-            )
-        except Exception as e:
-            logger.error(f"Error sending item {item_id} to inventory: {str(e)}")
-            self.retry(exc=e, countdown=10)
+        requests.patch(
+            inventory_response_url,
+            json={
+                "item_id": item_id,
+                "amount": amount
+            },
+            headers=headers
+        )
 
 
-@shared_task(bind=True, max_retries=3)
-def send_to_user_service(self, result_data, token):
+@shared_task
+def send_to_user_service(result_data, token):
     """Celery task for send result about gold to user"""
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
     inventory_response_url = f"{AUTH_API_URL}/api/v1/currencies/"
-    try:
-        gold = result_data.get("gold", 0)
-        if gold > 0:
-            requests.patch(
-                inventory_response_url,
-                json={
-                    "gold": gold,
-                    "guild_rage": 0
-                },
-                headers=headers
-            )
-    except Exception as e:
-        logger.error(f"Error sending gold to user service: {str(e)}")
-        self.retry(exc=e, countdown=10)
+    gold = result_data.get("gold", 0)
+    if gold > 0:
+        requests.patch(
+            inventory_response_url,
+            json={
+                "gold": gold,
+                "guild_rage": 0
+            },
+            headers=headers
+        )
 
 
 @shared_task(bind=True, max_retries=3)
@@ -142,27 +168,43 @@ def distribute_chest_opening_result(rewards, token):
     task_group.apply_async()
 
 
-@shared_task(bind=True, max_retries=3, autoretry_for=(Exception,), retry_backoff=True)
-def send_chest_event_to_kafka(self, chests, user_id):
+@shared_task
+def send_chest_event_to_kafka(chests, user_id):
     """Send chests open event to scoreboard Kafka"""
-    try:
-        producer = get_producer()
-        topic = settings.KAFKA_CHEST_EVENTS_TOPIC
-        for chest in chests:
-            try:
-                chest = {"user_id": user_id, "promo": chest[0], "exp": chest[1]}
-                serialized = json.dumps(chest, ensure_ascii=False).encode('utf-8')
-                producer.produce(topic, value=serialized)
-                logger.debug(f"Sent event: {chest}")
-            except Exception as e:
-                logger.error(f"Error: {str(e)}")
+    producer = get_producer()
+    topic = settings.KAFKA_CHEST_EVENTS_TOPIC
+    for chest in chests:
+        try:
+            chest = {"user_id": user_id, "promo": chest[0], "exp": chest[1]}
+            serialized = json.dumps(chest, ensure_ascii=False).encode('utf-8')
+            producer.produce(topic, value=serialized)
+            logger.debug(f"Sent event: {chest}")
+        except Exception as e:
+            logger.error(f"Error: {str(e)}")
 
-        producer.flush()
-        logger.info(f"Sent {len(chests)} events in Kafka")
-        return True
-    except Exception as e:
-        logger.error(f"Error: {str(e)}")
-        raise self.retry(exc=e, countdown=2 ** self.request.retries)
+    producer.flush()
+    logger.info(f"Sent {len(chests)} events in Kafka")
+
+
+@shared_task
+def recover_stuck_distribution():
+    time = timezone.now() - timedelta(minutes=10)
+
+    stuck_distributions = ChestOpeningDistribution.objects.filter(
+        updated_at__lt=time,
+        status__in=['pending', 'chest_used', 'rewards_generated', 'items_sent', 'gold_sent', 'kafka_sent']
+    )
+
+    for distribution in stuck_distributions:
+        open_chest_task.delay(distribution.id)
+
+    failed_distributions = ChestOpeningDistribution.objects.filter(
+        status='failed',
+        retry_count__lt=3
+    )
+
+    for distribution in failed_distributions:
+        open_chest_task.delay(distribution.id)
 
 
 @shared_task(bind=True)
